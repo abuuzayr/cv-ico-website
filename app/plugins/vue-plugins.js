bf6d165f4f73276fc68bda6716a32efc83adc92e
Abu Uzayr <abu.uzayr@builtforfifty.com>
2018-01-10 01:38:42 +0800

    Tooltip error for form input - Add v-tooltip package - Import v-tooltip package into app - Add styling for tooltips - Convert error messages for registration page to tooltips - Convert error messages for login page to tooltips - Convert error messages for forgot password page to tooltips

--- app/plugins/vue-plugins.js
+++ app/plugins/vue-plugins.js
@@ -4,7 +4,9 @@ import Notifications from 'vue-notification';
 import Prism from 'vue-prism';
 import Vuelidate from 'vuelidate';
 import Particles from '../components/Particles.vue';
+import VTooltip from 'v-tooltip';
 
 Vue.use(Notifications);
 Vue.use(Particles, Prism);
 Vue.use(Vuelidate);
+Vue.use(VTooltip);
