152b4c5e9598f657cd57fb8c67ea12066c6b4f32
Abu Uzayr <abu.uzayr@builtforfifty.com>
2018-01-17 11:01:24 +0800

    Handle 0 values

--- app/plugins/filters.js
+++ app/plugins/filters.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 
 function numFormat(value) {
-  if (!value) return '';
+  if (value === 0) {
+    return 0;
+  } else if (!value) {
+    return '';
+  }
   value = value.toString();
   value = value.replace(new RegExp('^(\\d{' + (value.length%3? value.length%3:0) + '})(\\d{3})', 'g'), '$1 $2').replace(/(\d{3})+?/gi, '$1 ').trim();
   value = value.replace(/\s/g, ',');
